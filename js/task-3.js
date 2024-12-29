c54990d08d0a31177eee1933582b13cc659fa78e
habibe <habibecinar07@gmail.com>
2024-12-29 17:34:53 +0300

    totalwidth

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,10 @@ function getElementWidth(content, padding, border) {
   const contentNumber = parseFloat(content);
   const paddingNumber = parseFloat(padding);
   const borderNumber = parseFloat(border);
-  const totalPrice = 2 * (paddingNumber + borderNumber) + contentNumber;
 
-  return parseFloat(totalPrice);
+  const totalWidth = 2 * (paddingNumber + borderNumber) + contentNumber;
+
+  return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
