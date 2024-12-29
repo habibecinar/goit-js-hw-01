2d4b0157dd785512c24cc047986d8f2c9bfb34be
habibe <habibecinar07@gmail.com>
2024-12-29 17:27:13 +0300

    box-sizing

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@ function getElementWidth(content, padding, border) {
   const borderNumber = parseFloat(border);
   const totalPrice = 2 * (paddingNumber + borderNumber) + contentNumber;
 
-  return totalPrice;
+  return parseFloat(totalPrice);
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
